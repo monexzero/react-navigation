64525e2afd04703b4b7dc97317f3f22c9e40b87b
gnk-sato-mnx <g.sato@isdajp.com>
2018-04-27 18:05:06 +0900

    add

--- scripts/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-case $CIRCLE_NODE_INDEX in 
-  0) yarn test && yarn codecov ;; 
-  1) cd examples/NavigationPlayground && yarn && yarn test ;; 
-  #2) cd examples/ReduxExample && yarn && yarn test ;; 
-esac
